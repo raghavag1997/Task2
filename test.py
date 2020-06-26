7f7abc7cf30def4109a5c116e6c3b11bb58dc35d
raghavag1997 <53235979+raghavag1997@users.noreply.github.com>
2020-06-26 10:32:40 +0530

    Chnage made by me

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import subprocess
-x=subprocess.getoutput("ls -l| grep index | grep html | awk '{print($9)}'")
+x=subprocess.getoutput("ls -l /root/.jenkins/workspace/Task-2-job1 | grep index | grep html | awk '{print($9)}'")
 x=x.replace('\n',"")
 if x=="index.html":
     print("yes")
